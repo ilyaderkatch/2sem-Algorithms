d968df83b207850b59e18b0cb79a9dd2ec9fc0a1
ilyaderkatch <47671444+ilyaderkatch@users.noreply.github.com>
2019-05-02 20:22:33 +0300

    Update MST2.h

--- task 3/Problem B2/MST2.h
+++ task 3/Problem B2/MST2.h
@@ -3,20 +3,7 @@
 
 #include "MatrixGraph.h"
 #include "ListGraph.h"
-#include <stack>
-#include <algorithm>
 
-using std::stack;
-using std::sort;
-
-vector <int> EulerCycle(const ListGraph &A);
-double MinCycle(const MatrixGraph &A);
-ListGraph MinMatching(const MatrixGraph &G, const vector <int> &A);
-vector <int> FindOddVertices(const ListGraph &G);
-bool NextSequence(vector <int> &A);
-void swap(vector <int> &A, int i, int j);
 ListGraph Prim(const MatrixGraph &A);
-double CorrectMinCycle(const MatrixGraph &A);
-
 
 #endif
