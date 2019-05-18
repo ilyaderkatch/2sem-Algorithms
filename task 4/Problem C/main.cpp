8fb74954d25626ea2621f55567203e2a917f6d79
ilyaderkatch <47671444+ilyaderkatch@users.noreply.github.com>
2019-05-18 13:53:24 +0300

    Update main.cpp

--- task 4/Problem C/main.cpp
+++ task 4/Problem C/main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <algorithm>
-#include <set>
 
 using std::vector;
 using std::cin;
 using std::cout;
 using std::endl;
-using std::pair;
-using std::min;
-using std::max;
-using std::sort;
-using std::set;
 
+int log(int n) {
+    if (n == 1) {
+        return 0;
+    }
+    return log(n / 2) + 1;
+}
 
 class LCA{
 private:
@@ -25,9 +23,7 @@ private:
 
 public:
     LCA(long long n);
-    void Out();
     long long FindParent(long long u, long long v) const;
-
 };
 
 void LCA::UpdateDepth(long long k)
@@ -61,49 +57,33 @@ long long LCA::FindParent(long long u, long long v) const
             diff /= 2;
             deg += 1;
         }
-        //cout << u << " " << v << endl;
     }
     if (v == u)
     {
         return v;
     }
-    for (int i = int(log2(Parents.size())); i >= 0; --i)
+    for (int i = int(log(Parents.size())); i >= 0; --i)
     {
         if (GrandParents[v][i] != GrandParents[u][i])
         {
             v = GrandParents[v][i];
             u = GrandParents[u][i];
         }
-       // cout << u << " " << v << endl;
     }
     return Parents[v];
-}
-
-void LCA::Out()
-{
-    for (long long i = 0; i < GrandParents.size(); ++i)
-    {
-        for (long long j = 0; j < GrandParents[i].size(); ++j)
-        {
-            cout << GrandParents[i][j] << " ";
-        }
-        cout << endl;
-    }
 }
 
 LCA::LCA(long long n)
 {
     //Обновляем Родителей
-    Parents.resize(0);
     Parents.push_back(0);
     for (long long i = 0; i < n - 1; ++i)
     {
         long long a;
-        cin >> a;
+        std::cin >> a;
         Parents.push_back(a);
     }
     //Обновляем глубину
-    Depth.resize(0);
     Depth.resize(n, -1);
     Depth[0] = 0;
     for (long long i = 0; i < Depth.size(); ++i)
@@ -120,16 +100,16 @@ LCA::LCA(long long n)
 void LCA::Predprocess()
 {
     long long n = Parents.size();
-    GrandParents.resize(n, vector<long long>(int(log2(n))));
+    GrandParents.resize(n, vector<long long>(log(n)));
     for (long long i = 0; i < n; ++i)
     {
         GrandParents[i][0] = Parents[i];
     }
-    for (long long i = 1; i < log2(n); ++i)
+    for (long long i = 1; i < log(n); ++i)
     {
         for (long long j = 1; j < n; ++j)
         {
-            GrandParents[j][i] = GrandParents[GrandParents[j - 1][i - 1]][i - 1];
+            GrandParents[j][i] = GrandParents[GrandParents[j][i - 1]][i - 1];
         }
     }
 }
@@ -147,7 +127,6 @@ int main()
     long long last = 0;
     for (long long i = 0; i < m; ++i)
     {
-        //cout << (a + last) % n << " " << b << " " << A.FindParent((a + last) % n, b) << endl;
         last = A.FindParent((a + last) % n, b);
         result += last;
         a = (x * a + y * b + z) % n;
